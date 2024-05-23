f7e56239cfa397a11a2919b781c7048e81219db2
Arnab <arnabmondal604@gmail.com>
2024-05-23 09:14:35 +0530

    security implemented

---
+++ APIGateway/APIGateway/src/main/java/com/gateway/models/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.gateway.models;
+
+import lombok.*;
+
+import java.util.Collection;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+    private long expireAt;
+    private Collection<String> authorities;
+}
