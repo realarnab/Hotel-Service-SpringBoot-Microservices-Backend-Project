7cef4eb55d56b5ddea486b8ebf67bec888233535
Arnab <arnabmondal604@gmail.com>
2024-05-26 00:57:13 +0530

    bean defined error fixed

--- UserService/UserService/src/main/java/com/userservice/config/interceptor/RestTemplateInterceptor.java
+++ UserService/UserService/src/main/java/com/userservice/config/interceptor/RestTemplateInterceptor.java
@@ -1,5 +1,7 @@
 package com.userservice.config.interceptor;
 
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -12,6 +14,7 @@ import java.io.IOException;
 
 public class RestTemplateInterceptor implements ClientHttpRequestInterceptor {
 
+    @Autowired
     private OAuth2AuthorizedClientManager manager;
 
     public RestTemplateInterceptor(OAuth2AuthorizedClientManager manager) {
